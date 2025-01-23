8ad1b46c09bceee358b4bd32a096e2a9b4d362f4
velzeus <galepegasus@gmail.com>
2025-01-23 19:00:02 +0900

    Sceneを移行

---
+++ DirectXInit/SceneFolder/SceneManager.h
@@ -0,0 +1,60 @@
+#pragma once
+#include"Scene.h"
+#include<vector>
+#include<algorithm>
+#include"sound.h"
+
+//前方宣言
+class Scene;
+
+enum  SCENENAME
+{
+	TITLE=0,
+
+
+	RESULT,
+
+	SCENENAME_MAX,
+};
+
+class SceneManager
+{
+private:
+	static SceneManager* instance;// インスタンス
+
+	Scene* currentScene;// 現在のシーン
+	Scene* nextScene;//次のシーン
+
+	// サウンド
+	Sound* sound;
+
+	std::vector<Scene*> scenes;
+	static bool endFlg;
+
+	//コンストラクタ
+	SceneManager();
+
+public:
+	
+
+	//インスタンスを取得
+	static SceneManager* GetInstance();
+
+	//シーンを追加
+	void AddScene(Scene* _scene);
+
+	//各シーンに関する処理を呼び出す
+	int Start();	//開始処理
+	int Update();	//更新処理
+	int Draw();		//描画処理
+	int End();		//終了処理
+
+
+	//シーンを切り替える
+	void ChangeScene(SCENENAME _sceneName);
+};
+
+//静的メンバ変数の定義
+// SceneManager* SceneManager::instance = nullptr;
+// bool SceneManager::endFlg = false;
+
